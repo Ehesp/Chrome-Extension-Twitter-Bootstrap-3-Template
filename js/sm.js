6542bc3ef3c49fdc5ce352064c1bf797e5326f44
Denis Haskin <denis@constantorbit.com>
2017-01-24 16:09:50 -0800

    Chrome extension -- Add adds it to the list and storage, upload gets the list from storage and clears it.

--- js/sm.js
+++ js/sm.js
@@ -1,1 +1,36 @@
-alert("hi mom!");
+$( document ).ready(function() {
+  chrome.storage.local.get({urls: []}, function(data) {
+    data.urls.map(function(url) {
+      $('#urls').append($('<div>' + url + '</div>'));
+    });
+  });
+
+  $('.client-list-item').on('click', function(e) {
+    console.log("Clicked ", e);
+  });
+
+  $('#add-url').on('click', function(e) {
+    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+        var url = tabs[0].url;
+        chrome.storage.local.get({urls: []}, function(data) {
+          var urls = data.urls;
+          urls.push(url);
+          chrome.storage.local.set({urls: urls}, function() {
+            $('#urls').append($('<div>' + url + '</div>'));
+          });
+        });
+    });
+  });
+
+  $('#import').on('click', function(e) {
+    chrome.storage.local.get({urls: []}, function(data) {
+      var urls = data.urls;
+      console.log("We'll upload ", urls)
+      chrome.storage.local.set({urls: []}, function() {
+        $('#urls').empty();
+      });
+    });
+  });
+
+
+});
