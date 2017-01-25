979508581ca3cc26f50cb4c5d6f9b5284134b7f2
Denis Haskin <denis@constantorbit.com>
2017-01-24 16:54:14 -0800

    Uploads successfully, but to a hardcoded URL.
    Committing for now so I can do a branch for local and a branch for production.

--- js/sm.js
+++ js/sm.js
@@ -26,6 +26,12 @@ $( document ).ready(function() {
     chrome.storage.local.get({urls: []}, function(data) {
       var urls = data.urls;
       console.log("We'll upload ", urls)
+      // http://localhost:3000/clients/392/stories/1946/article_batches
+      $.post("http://localhost:3000/clients/392/stories/1946/article_batches.json",
+        {article_batch: {name: 'From extension', story_id: '1946', urls_array: urls}}, function(data, textStatus) {
+          console.log(data);
+          console.log(textStatus);
+        });
       chrome.storage.local.set({urls: []}, function() {
         $('#urls').empty();
       });
